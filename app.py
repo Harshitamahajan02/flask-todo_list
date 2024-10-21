9cd3a1312350f1cb34ca4a9ae0cc2093a3fbfed0
Harshitamahajan02 <mahajanharshu02@gmail.com>
2024-10-21 23:59:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 
-from flask import Flask, render_template, request, redirect
+f
+flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
